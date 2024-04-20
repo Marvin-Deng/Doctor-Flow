9992dc2e253c25d424c498d5543d18c7f06b3d10
GeneKung <52974083+GeneKung@users.noreply.github.com>
2024-04-20 12:32:16 -0700

    Auth0 stuff

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const { connectToMongoDB } = require('./config/db'); 
+const https = require('https');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 const patientRoutes = require('./routes/patientRoutes');
 
 const { auth } = require('express-openid-connect');
+const { requiresAuth } = require('express-openid-connect');
 
 const config = {
   authRequired: false,
   auth0Logout: true,
-  secret: 'Eu4LLF4/h/zNAvzzqB16oQmOI4fVrWyQ6uIbLMh0I1M=',
-  baseURL: 'http://localhost:3000/login',
+  secret: 'ObFjYyh1DxAWcvyIUWXphsuDxwG7/+1u0jBIQ62sHRc=',
+  baseURL: 'https://localhost:3000',
   clientID: 'd3AG5pNNKpHPll1aRjqypWq8R5vn6NiS',
   issuerBaseURL: 'https://dev-xdx10w8664v1a2yz.us.auth0.com'
 };
+
+// Middleware for parsing request bodies (you can choose between body-parser or express.json())
+app.use(express.json());
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
@@ -22,20 +28,19 @@ app.use(auth(config));
 app.get('/', (req, res) => {
   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
-app.all('/login/callback', (req, res) => {
-  res.send('Callback route reached');
-});
-
-// Middleware for parsing request bodies (you can choose between body-parser or express.json())
-app.use(express.json());
 
 // Start Express app after connecting to MongoDB
 connectToMongoDB()
   .then(() => {
     // Add routes and other configurations here
-    app.use('/patients', patientRoutes);
-
-    app.listen(port, () => {
+    app.use('/patients', requiresAuth(), patientRoutes); // Apply requiresAuth middleware to patient routes
+    // Configure HTTPS server
+    const httpsOptions = {
+      key: fs.readFileSync('server.key'),
+      cert: fs.readFileSync('server.cert')
+    };
+    
+    https.createServer(httpsOptions, app).listen(port, () => {
       console.log("Server is running on port", port);
     });
   })
