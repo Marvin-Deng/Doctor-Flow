8a7289993385ce6a192cb3be3724ced11f1ec855
Marvin Deng <52214624+Marvin-Deng@users.noreply.github.com>
2024-04-20 03:15:52 -0700

    New patient modal (#13)
    * Change patient grid layout
    
    * Add new patient form to modal

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,8 +1,19 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import NewPatientForm from "../../components/NewPatientForm";
-import PatientList from "../../components/PatientList"
+import PatientList from "../../components/PatientList";
 
 const LandingPage = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsOpen(false);
+  };
+
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center items-center">
       <header className="w-full bg-white shadow">
@@ -10,8 +21,14 @@ const LandingPage = () => {
           <h1 className="text-3xl font-bold text-gray-900">Doctor-Flow</h1>
         </div>
       </header>
+      <button
+        className="fixed right-4 top-20 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+        onClick={handleOpenModal}
+      >
+        Add New Patient
+      </button>
       <PatientList />
-      <NewPatientForm />
+      <NewPatientForm isOpen={isOpen} handleClose={handleCloseModal} />
     </div>
   );
 };
