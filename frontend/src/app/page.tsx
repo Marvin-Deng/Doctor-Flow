4c3d7b21bc26ccb8087dd8a1adb656364a71fe4d
Marvin-Deng <52214624+Marvin-Deng@users.noreply.github.com>
2024-04-19 23:31:54 -0700

    Add medtable

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import MedTable from "../components/MedTable"
 import NewPatientForm from "../components/NewPatientForm"
 
 export default function Home() {
   return (
     <main>
+      <MedTable />
       <NewPatientForm />
     </main>
   );
